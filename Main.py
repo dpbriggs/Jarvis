13830b816f3e2734a5c99dd0aa506b61e7645439
Artyom <jomojules265@gmail.com>
2013-12-06 20:38:41 -0600

    Crappy subpod formatting.

--- Main.py
+++ Main.py
@@ -29,9 +29,19 @@ class output:
     #Call espeak
     def callespeak(inputstr):
         proc=subprocess.Popen(['espeak',str(inputstr)])
-
-
-class accuracy:
+    
+            
+class processing:
+    
+    def weatherinfo(tempurature, windchill, conditions):
+        hold = [tempurature, windchill, conditions]
+        print(hold)
+        for i in range(0, len(hold)-1):
+            if hold[i][0] == "-":
+                hold[i] = "negative " + hold[i][1:]
+                
+        output = "It is " + hold[0] + " Degrees with a Windchill of " + hold[1] + " and the skys are" + hold[2]
+        return output
     
     def numkeywords(input, output):
         inputterms = input.split(' ')
@@ -42,19 +52,9 @@ class accuracy:
                 hold += 1
         return hold
     #print(accuracy('this is an cat', 'this is a dog'))
-
-class format:
-    def weatherinfo(tempurature, windchill, conditions):
-        hold = [tempurature, windchill, conditions]
-        print(hold)
-        for i in range(0, len(hold)-1):
-            if hold[i][0] == "-":
-                hold[i] = "negative " + hold[i][1:]
-
-        output = "It is " + hold[0] + " Degrees with a Windchill of " + hold[1] + " and the skys are" + hold[2]
-        return output
     
 class data:
+    
     
     def getweather():
         url = urllib.request.urlopen(config[0]).read()
@@ -74,39 +74,47 @@ class data:
                 conditions = text[i][len('Sky conditions:'):]
         if nowindchill != 1:
             windchill = 0
-        outdata = format.weatherinfo(str(celsius), str(windchill), conditions)
+        outdata = processing.weatherinfo(str(celsius), str(windchill), conditions)
         output.callespeak(outdata)
 
-    matchingkey = config[2]
  
     def wolframinfo(urlInput):
         url = 'http://api.wolframalpha.com/v2/query?input="' + urlInput.replace(" ", "%20") + '"&appid=' + config[1]
         wolfinfo = urllib.request.urlopen(url).read()
-        xmltext = wolfinfo.decode('utf-8')
-        xmltext = xmltext.split('</pod>')
-        subxmltext = xmltext.split('</subpod>')
-        numpods = int(xmltext[0][xmltext[0].find('numpods=')+9:xmltext[0].find('datatypes=')-6])
-        #if numsubpods >= 1:
-        numsubpods = int(xmltext[0][xmltext[0].find('numpods=')+9:xmltext[0].find('datatypes=')-6])
+        xmltext = wolfinfo.decode('utf-8') #Decodes from binary
+        xmltext = xmltext.split('</pod>') # Splits Pod
+        numsubpod = []
+        numpods = xmltext[0][xmltext[0].find('numpods=')+9:xmltext[0].find('datatypes=')-1].rstrip('"')
+        for i in range(0, len(xmltext)):
+            numsubpod.append(xmltext[i][xmltext[i].find('numsubpods='):xmltext[i].find('datatypes=')-1].rstrip('"'))
+        return
+        print (numsubpod)
+        podinfo = []
         info = {}
-        for i in range(1, numpods):
-            podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
-            podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
-            info[i] = (podtitle, podinfo)
-        return info
 
-config = input.readconfig()
-#print(config)
-urlInput = "what is eulors number?"
-
-urlInput2 = "how many m and m's could fit in a bath tub?"
-            
-
+        for i in range(1, numpods): #Info Proc
+            if numsubpod == 1:
+                podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
+                podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
+                info[i] = (podtitle, podinfo,)
+            if numsubpod > 1:
+                podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
+                podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
+                subpodtitle = xmlsubtext[i][xmlsubtext[i].find('<subpod title="')+len('<subpod title="')+3:xmlsubtext[i].find('scanner')-7].split('|')
+                subpodinfo = xmlsubtext[i][xmlsubtext[i].find('<plaintext>')+len('<plaintext>'):xmlsubtext[i].find('</plaintext>')].split('|')
+                info[i] = (podtitle,(subpodtitle,subpodinfo))
+        return  info
         
 
 
 
+config = input.readconfig()
+#print(config)
+#urlInput = "what is the weather in ajax?"
 
+print(data.wolframinfo("How many m and m's could fit in a bathtub."))
+
+#urlInput = "what is eulors number?"
 
 
 
