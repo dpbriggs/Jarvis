dedb75658dd8f9266e5cc6bb71a6e971e2f4a1fd
Artyom <jomojules265@gmail.com>
2013-12-09 15:40:44 -0600

    Added Date and Time

--- Main.py
+++ Main.py
@@ -1,68 +1,18 @@
 #!/usr/bin/env python3
 #Import everything
 import subprocess
-import urllib.request 
+import urllib.request
+from datetime import datetime
 
-<<<<<<< HEAD
-## MAJOR CLASSES: input, output, format, and data
-## EACH CLASS DEALS NAME
-## CLASSES YET TO BE MADE:
-=======
 ## MAJOR CLASSES: input, output, processing, and data
 ## EACH CLASS DEALS WITH NAME
 ## THINGS YET TO BE MADE:
->>>>>>> refs/remotes/origin/master
 ##      NATURAL LANGUAGE OUTPUT
 ##      INPUT OF ANY KIND
 ## CLASSES YET TO BE EXPANDED:
 ##      ACCURACY
 ##      ETC (We'll expand when we discuss)
 
-<<<<<<< HEAD
-    
-class input:
-    def readconfig():
-        #load config file and edit it
-        tempconfig = open("config.txt").readlines()
-        config = []
-        for i in range(0, len(tempconfig)):  #elements in array
-            if tempconfig[i].find('#') == -1 and tempconfig[i] != "\n":
-                config.append(tempconfig[i].rstrip('\n'))
-        config[3] = config[3].split(',') #seperate keywords
-        return config
-
-    
-class output:
-    #Call espeak
-    def callespeak(inputstr):
-        proc=subprocess.Popen(['espeak',str(inputstr)])
-    
-            
-class processing:
-    
-    def weatherinfo(tempurature, windchill, conditions):
-        hold = [tempurature, windchill, conditions]
-        print(hold)
-        for i in range(0, len(hold)-1):
-            if hold[i][0] == "-":
-                hold[i] = "negative " + hold[i][1:]
-                
-        output = "It is " + hold[0] + " Degrees with a Windchill of " + hold[1] + " and the skys are" + hold[2]
-        return output
-    
-    def numkeywords(input, output):
-        inputterms = input.split(' ')
-        outputterms = output.split(' ')
-        hold = 0
-        for i in inputterms:
-            if i in outputterms:
-                hold += 1
-        return hold
-    #print(accuracy('this is an cat', 'this is a dog'))
-    
-class data:
-    
-=======
 ## INCONSISTENCIES IN THE WOLFRAMALPHA XML FILE MAKE SUBPOD SUPPORT VERY DIFFICULT
 ## WILL DISCUSS WITH YOU GUYS LATER
 
@@ -134,7 +84,24 @@ class processing:
         
     
 class data:
->>>>>>> refs/remotes/origin/master
+
+    def timeanddate(i):
+        now = datetime.now()
+        current_year = str(now.year)
+        current_month = str(now.month)
+        current_day = str(now.day)
+        current_hour = str(now.hour)
+        current_minute = str(now.minute)
+        month = ['january' , 'february', 'march' , 'april', 'may', 'june', 'july', 'augest', 'september', 'october', 'november', 'december']
+
+        if i == 1:
+            date = ((month[now.month-1]),now.day,now.year)
+            output.callespeak(date)
+        elif i == 2:
+            time = (now.hour,now.minute)
+            output.callespeak(time)
+
+
     
     def getweather():
         url = urllib.request.urlopen(config[0]).read()
@@ -155,48 +122,6 @@ class data:
         if nowindchill != 1:
             windchill = 0
         outdata = processing.weatherinfo(str(celsius), str(windchill), conditions)
-<<<<<<< HEAD
-        output.callespeak(outdata)
-
- 
-    def wolframinfo(urlInput):
-        url = 'http://api.wolframalpha.com/v2/query?input="' + urlInput.replace(" ", "%20") + '"&appid=' + config[1]
-        wolfinfo = urllib.request.urlopen(url).read()
-        xmltext = wolfinfo.decode('utf-8') #Decodes from binary
-        xmltext = xmltext.split('</pod>') # Splits Pod
-        numsubpod = []
-        numpods = xmltext[0][xmltext[0].find('numpods=')+9:xmltext[0].find('datatypes=')-1].rstrip('"')
-        for i in range(0, len(xmltext)):
-            numsubpod.append(xmltext[i][xmltext[i].find('numsubpods='):xmltext[i].find('datatypes=')-1].rstrip('"'))
-        return
-        print (numsubpod)
-        podinfo = []
-        info = {}
-
-        for i in range(1, numpods): #Info Proc
-            if numsubpod == 1:
-                podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
-                podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
-                info[i] = (podtitle, podinfo,)
-            if numsubpod > 1:
-                podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
-                podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
-                subpodtitle = xmlsubtext[i][xmlsubtext[i].find('<subpod title="')+len('<subpod title="')+3:xmlsubtext[i].find('scanner')-7].split('|')
-                subpodinfo = xmlsubtext[i][xmlsubtext[i].find('<plaintext>')+len('<plaintext>'):xmlsubtext[i].find('</plaintext>')].split('|')
-                info[i] = (podtitle,(subpodtitle,subpodinfo))
-        return  info
-        
-
-
-
-config = input.readconfig()
-#print(config)
-#urlInput = "what is the weather in ajax?"
-
-print(data.wolframinfo("How many m and m's could fit in a bathtub."))
-
-#urlInput = "what is eulors number?"
-=======
         return outdata
 
 
@@ -221,6 +146,7 @@ print(data.wolframinfo("How many m and m's could fit in a bathtub."))
             podtitle = xmltext[i][xmltext[i].find('<pod title="')+len('<pod title="')+3:xmltext[i].find('scanner')-7].split('|')
             podinfo = xmltext[i][xmltext[i].find('<plaintext>')+len('<plaintext>'):xmltext[i].find('</plaintext>')].split('|')
             info.append((podtitle, podinfo))
+        print(info)
         return processing.readwolframinfo(info)
 
  
@@ -256,6 +182,7 @@ def parseinput(inputx, keywords, functions):
     
 #print('search '+urlInput)
 #parseinput('weather', keywords, functions)
+data.timeanddate(2)
 inputx = str(input('Enter Question: '))
 parseinput(inputx, keywords, functions)
 
@@ -264,7 +191,6 @@ parseinput(inputx, keywords, functions)
 
 
 
->>>>>>> refs/remotes/origin/master
 
 
 
